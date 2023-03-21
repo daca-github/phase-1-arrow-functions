85173c7ac13ae385261eb0d0fa0490a2f43300f2
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-03-21 19:43:33 -0400

    Complete

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function (num1,num2){
+    return num1 / num2
+}
+
+const square = multiplication => multiplication * multiplication; 
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
